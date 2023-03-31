2c302bc0187a4375b5a649f9c1765688853b549b
Anton <>
2023-03-31 08:59:41 +0200

    Add project files.

---
+++ Array Inlämningsuppgift 5.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] frågor = { "Vad heter Sveriges huvudstad? ", "Vilken är Sveriges största stad? ", "Vilken stad i Sverige börjar på V och slutar på äxjö? ", "Vad heter Norges huvudstad? " };
+            string[] FrågorSvar = { "Stockholm", "Stockholm", "Växjö", "Oslo" };
+
+            Console.Write("Välj en fråga mellan 1-4 : ");
+            int VilkenFråga=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Fråga "+VilkenFråga+" : " + frågor[VilkenFråga-1]); 
+            string Svar=Console.ReadLine();
+
+            if (Svar == FrågorSvar[VilkenFråga-1])
+            {
+                Console.WriteLine("Rätt!"); 
+            }
+
+            else
+                Console.WriteLine("Fel!");
+
+
+        }
+    }
+}
